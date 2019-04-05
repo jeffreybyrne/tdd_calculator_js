f28190c68d323c56fef6bc890754e3810cee0f66
Jeff Byrne <jeffrey.byrne@gmail.com>
2019-04-05 13:57:04 -0400

    finished the exercise

--- calculator.js
+++ calculator.js
@@ -1,10 +1,15 @@
-function add() {
+function add(num1, num2) {
+    return num1 + num2;
 }
 
-function subtract() {
+function subtract(num1, num2) {
+    return num1 - num2;
 }
 
-function sum() {
+function sum(array) {
+    fTotal = array.reduce((total, element) => total + element, 0);
+
+    return fTotal;
 }
 
 module.exports = {add, subtract, sum}
